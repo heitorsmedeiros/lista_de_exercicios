e48493494b9795d1465410c86095a158e42f2a2b
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-08-28 15:59:16 -0300

    feat: Exercício 1, Nível 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
+ /*
+Nível 1: Casting implícito e explícito
 
+1.	Casting Implícito: Declare uma variável do tipo int e atribua a ela um valor. Em seguida, crie uma variável do tipo double e atribua a ela o valor da variável int. Exiba o valor da variável double no console
+  */
 
 public class Main {
     public static void main() {
+        int valorInt = 7;
+        double valorDouble = valorInt; //casting implícito
+        System.out.println(valorDouble);
         }
 }
