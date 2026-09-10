81436ab1280575286f82d86a39b7030c134b50bd
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-09-10 15:39:39 -0300

    feat: Exercício Aula 10/09/2026 - Regra Curso de Idiomas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,22 @@
 package org.example;
  /*
- Exercício Aula 10/09/2026 - Regra de Auxílio Combustível
+ Exercício Aula 10/09/2026 - Regra Curso de Idiomas
 
   */
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("O funcionário utiliza veículo próprio para trabalhar? (S/N): ");
-        String resposta = scanner.nextLine();
+        System.out.print("Digite o cargo do funcionário: ");
+        String cargo = scanner.nextLine();
         
-        if(resposta.equalsIgnoreCase("S")) {
-            System.out.println("Ele pode solicitar auxílio combustível.");
+        if (cargo.equalsIgnoreCase("administrativo") || cargo.equalsIgnoreCase("liderança") || cargo.equalsIgnoreCase("lideranca")) {
+            System.out.println("O funcionário pode participar do programa de idiomas.");
         } else {
-            System.out.println("Não possui auxílio disponível");
+            System.out.println("Não pode participar do programa de idiomas.");
         }
     }
 }
