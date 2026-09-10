0ee28d6e14d28ef88a8af918dc45a3cfe8cde18e
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-09-10 15:41:28 -0300

    feat:  Exercício Aula 10/09/2026 - Regra Investimentos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,24 @@
 package org.example;
  /*
- Exercício Aula 10/09/2026 - Regra Empréstimo
+ Exercício Aula 10/09/2026 - Regra Investimentos
 
   */
 
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double salario = scanner.nextDouble();
-        double parcela = scanner.nextDouble();
+        double valorInvestido = scanner.nextDouble();
         
-        if(parcela <= (salario * 0.30)) {
-            System.out.println("Empréstimo aprovado");
+        if(valorInvestido <= 10000) {
+            System.out.println("Bronze");
+        } else if(valorInvestido <= 50000) {
+            System.out.println("Prata");
+        } else if(valorInvestido <= 100000) {
+            System.out.println("Ouro");
         } else {
-            System.out.println("Empréstimo recusado");
+            System.out.println("Platinum");
         }
     }
 }
