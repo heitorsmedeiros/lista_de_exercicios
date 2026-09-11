56d8e077ee87e1df9f4cee170898b8a38c1185f6
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-09-11 14:13:04 -0300

    feat: Exercício Aula 11/09/2026 - Controle de Produção Industrial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,13 @@
 package org.example;
  /*
-Exercício Aula 10/09/2026 - Controle de Eventos
+Exercício Aula 11/09/2026 - Controle de Produção Industrial
 
 */
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.print("Digite o nome do participante " + i + ": ");
-            String nome = scanner.nextLine();
-            System.out.println("Participante " + nome + " registrado com sucesso!\n");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Peça Produzida " + i);
         }
-
-        scanner.close();
     }
 }
