86e4ecb096cf24cc93470f114441e90af4a23879
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-09-11 14:57:11 -0300

    feat: Exercício Aula 11/09/2026 - Controle de Estoque

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,12 @@
 package org.example;
  /*
-Exercício Aula 11/09/2026 - Caixa de Supermercado 
+Exercício Aula 11/09/2026 - Controle de Estoque 
 */
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double total = 0;
-        String continuar;
-
-        do {
-            System.out.print("Digite o valor do produto: ");
-            double valor = scanner.nextDouble();
-            total += valor;
-
-            System.out.print("Deseja continuar registrando? (S/N): ");
-            continuar = scanner.next();
-        } while (continuar.equalsIgnoreCase("S"));
-
-        System.out.println("\nTotal da compra: R$ " + total);
-
-        scanner.close();
+        for (int i = 1; i <= 15; i++) {
+            System.out.println("Produto " + i + " Conferido");
+        }
     }
 }
